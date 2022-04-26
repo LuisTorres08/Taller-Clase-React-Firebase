d54d6d5cddc8df779927785495ecbfe78d87dabb
Luis Torres <luisdeavila010@gmail.com>
2022-04-25 21:05:05 -0500

    Creacion editar, validaciones, interfaz optimizada

--- src/components/Formulario.js
+++ src/components/Formulario.js
@@ -1,4 +1,3 @@
-
 import React, { useEffect, useState } from 'react'
 import {firebase} from '../firebase'
 
@@ -7,9 +6,12 @@ const Formulario = () => {
     const [nombre, setNombre] = useState('')
     const [apellido, setApellido ] = useState('')
     const [edad, setEdad] = useState('')
-    const [direccion, setDireccion] = useState('')
+    const [direccion,setDireccion] = useState('')
     const [telefono, setTelefono] = useState('')
     const [data, setData] = useState([])
+    const [modoEdicion, setModoEdicion] = useState(false)
+    const [id, setId] = useState('')
+    const [error, setError] = useState(null)
 
     useEffect(()=>{
 
@@ -19,7 +21,7 @@ const Formulario = () => {
                 const data = await db.collection('registro').get()
                 const array = data.docs.map(item =>(
                     {
-                    id:item.id,...item.data()
+                        id:item.id, ...item.data()
                     }
                 ))
                 setData(array)
@@ -35,6 +37,31 @@ const Formulario = () => {
     const insertar = async (e) =>{
         e.preventDefault()
 
+        if(!nombre.trim()){
+            setError('Campo nombre vacío')
+            return
+        }
+
+        if(!apellido.trim()){
+            setError('Campo apellido vacío')
+            return
+        }
+
+        if(!edad.trim()){
+            setError('Campo edad vacío')
+            return
+        }
+
+        if(!direccion.trim()){
+            setError('Campo direccion vacío')
+            return
+        }
+
+        if(!telefono.trim()){
+            setError('Campo telefono vacío')
+            return
+        }
+
         try{
             const db = firebase.firestore()
             const nuevaData = {
@@ -42,11 +69,11 @@ const Formulario = () => {
                 nombreapellido:apellido,
                 nombreAño:edad,
                 nombredireccion:direccion,
-                nombretelefono:telefono,
+                nombretelefono:telefono
             }
             await db.collection('registro').add(nuevaData)
             setData([...data,
-                {
+                {   
                     nombrenombre: nombre,
                     nombreapellido: apellido,
                     nombreAño:edad,
@@ -63,6 +90,8 @@ const Formulario = () => {
         setEdad('')
         setDireccion('')
         setTelefono('')
+        setModoEdicion(false)
+        setError(null)
     }
 
     const eliminar= async (id) =>{
@@ -75,6 +104,81 @@ const Formulario = () => {
             console.log(error)
         }
     }
+    
+
+    const editar = (item) =>{
+        setNombre(item.nombrenombre)
+        setApellido(item.nombreapellido)
+        setEdad(item.nombreAño)
+        setDireccion(item.nombredireccion)
+        setApellido(item.nombretelefono)
+        setModoEdicion(true)
+        setId(item.id)
+    }
+
+    const editarRegistro = async e =>{
+        e.preventDefault()
+
+        
+       
+        if(!nombre.trim()){
+            setError('Campo nombre vacío')
+            return
+        }
+
+        if(!apellido.trim()){
+            setError('Campo apellido vacío')
+            return
+        }
+
+        if(!edad.trim()){
+            setError('Campo edad vacío')
+            return
+        }
+
+        if(!direccion.trim()){
+            setError('Campo direccion vacío')
+            return
+        }
+
+        if(!telefono.trim()){
+            setError('Campo telefono vacío')
+            return
+        }
+       
+        try{
+            const db= firebase.firestore()
+            await db.collection('registro').doc(id).update({
+                nombrenombre: nombre,
+                nombreapellido: apellido,
+                nombreAño:edad,
+                nombredireccion:direccion,
+                nombretelefono:telefono
+            })
+
+           
+        }catch(error){
+            console.log(error)
+        }
+        setNombre('')
+        setApellido('')
+        setEdad('')
+        setDireccion('')
+        setTelefono('')
+        setModoEdicion(false)
+        setError(null)
+
+    }
+
+    const cancelar =()=>{
+        setNombre('')
+        setApellido('')
+        setEdad('')
+        setDireccion('')
+        setTelefono('') 
+        setModoEdicion(false)
+        setError(null)
+    }
 
     return (
         <div className='container mt-5'>
@@ -83,64 +187,119 @@ const Formulario = () => {
             <div className='row'>
 
             <div>
-                    <h4 className='text-center'>Insertar</h4>
-                    <form onSubmit={insertar}>
-                    <input
-                    className='form-control mb-2'
-                    type="text"
-                    placeholder='Ingrese nombre'
-                    onChange={(e)=>setNombre(e.target.value)}
-                    />
-                     <input
-                     className='form-control mb-2'
-                    type="text"
-                    placeholder='Ingrese apellido'
-                    onChange={(e)=>setApellido(e.target.value)}
-                    />
-                     <input
-                     className='form-control mb-2'
-                    type="number"
-                    placeholder='Ingrese edad'
-                    onChange={(e)=>setEdad(e.target.value)}
-                    />
-                     <input
-                     className='form-control mb-2'
-                    type="text"
-                    placeholder='Ingrese direccion'
-                    onChange={(e)=>setDireccion(e.target.value)}
-                    />
-                     <input
-                     className='form-control mb-2'
-                    type="text"
-                    placeholder='Ingrese telefono'
-                    onChange={(e)=>setTelefono(e.target.value)}
-                    />
-                    <button type='submit'>Agregar</button>
+                    <h4 className='text-center'> 
+                    {
+                        modoEdicion ? 'Editar registro': 'Agregar registro'
+                    }
+                    </h4>
+
+                    <form onSubmit={modoEdicion ? editarRegistro: insertar}>
+                    {
+                     error ? <span className='text-danger'>{error}</span> : null
+                    }
+                    <div className='row'>
+                        <div className='col-4'>
+                         <label>Nombre</label>
+                            <input
+                            className='form-control mb-2'
+                            type="text"
+                            placeholder='Ingrese el nombre'
+                            onChange={(e)=>setNombre(e.target.value)}
+                            />
+                        </div>
+
+                        <div className='col-4'>
+                         <label>Apellido</label>
+                            <input
+                            className='form-control mb-2'
+                            type="text"
+                            placeholder='Ingrese el apellido producción '
+                            onChange={(e)=>setApellido(e.target.value)}
+                            />
+                            
+                        </div>
+
+                        <div className='col-4'>
+                         <label>Edad</label>
+                            <input
+                            className='form-control mb-2'
+                            type="number"
+                            placeholder='Ingrese la edad producción '
+                            onChange={(e)=>setEdad(e.target.value)}
+                            />
+                            
+                        </div>
+
+                        <div className='col-4'>
+                         <label>Direccion</label>
+                            <input
+                            className='form-control mb-2'
+                            type="text"
+                            placeholder='Ingrese la direccion'
+                            onChange={(e)=>setDireccion(e.target.value)}
+                            />
+                            
+                        </div>
+
+                        <div className='col-4'>
+                         <label>Telefono</label>
+                            <input
+                            className='form-control mb-2'
+                            type="number"
+                            placeholder='Ingrese el telefono'
+                            onChange={(e)=>setTelefono(e.target.value)}
+                            />
+                            
+                        </div>
+
+                       
+                    </div>
+                   
+                     
+                    {
+                    !modoEdicion? (
+                        <button className='btn btn-success btn-block' type='submit'>Insertar</button>
+                     )
+                     :
+                     (  <>
+                        <button className='btn btn-info btn-block' type='submit'>Editar</button>
+                        <button className='btn btn-dark btn-block mx-2' onClick={() => cancelar()}>Cancelar</button>
+                        </>
+                     )
+                    }
 
                 </form>
+                <br/>
                 </div>
 
-                <div className="col text-center">
-                    <h4 className="text-center">Listado nombres</h4>
-                    <ul className="col-sm- list-group">
-                    {
-                        data.map((item, index)=>(
-                            <li className='list-group-item' key={index}>
-                                <span className='lead'>
-                                    {item.nombrenombre} - 
-                                    {item.nombreapellido} -
-                                    {item.nombreAño} -
-                                    {item.nombredireccion} -
-                                    {item.nombretelefono} -
-                                    </span>
-                                    <button className='btn btn-danger btn-sm float-end mx-2' onClick={()=> eliminar(item.id)}>Eliminar</button>
-                                <button className='btn btn-warning btn-sm float-end' >editar</button>
-                            </li>
+                <table className='table table-bordered'>
+                    <thead>
+                        <tr>
+                            <th>Nombre</th>
+                            <th>Apellido</th>
+                            <th>Edad</th>
+                            <th>Direccion</th>
+                            <th>Telefono</th>
+                            
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        {data.map(item => (
+                            <tr>
+                                <td>{item.nombrenombre}</td>
+                                <td>{item.nombreapellido}</td>
+                                <td>{item.nombreAño}</td>
+                                <td>{item.nombredireccion}</td>
+                                <td>{item.nombretelefono}</td>
+                                <td><button className='btn btn-danger btn-sm float-end mx-2' onClick={()=> eliminar(item.id)}>Eliminar</button></td>
+                                <td> <button className='btn btn-warning btn-sm float-end' onClick={()=> editar(item)} >editar</button></td>
+                            </tr>
                         ))
-                    }
-                    </ul>
-                </div>
-
+                        }
+                    </tbody>
+                </table>
+                
             </div>
         </div>
     )
@@ -148,3 +307,5 @@ const Formulario = () => {
 }
 
 export default Formulario
+
+
